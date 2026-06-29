ddf36f85591e59537d573716437cd1ae76c594fa
Mandervain <kmnski.l@gmail.com>
2026-06-29 17:12:44 +0200

    feat: add startup logging to all service Application classes

--- src/main/java/com/shop/catalog/CatalogApplication.java
+++ src/main/java/com/shop/catalog/CatalogApplication.java
@@ -1,12 +1,23 @@
 package com.shop.catalog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(CatalogApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CatalogApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner startupLogger() {
+        return args -> log.info("shop-catalog service started and ready to serve requests");
     }
 }
